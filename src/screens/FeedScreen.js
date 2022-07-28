bc387f1f9c641e648fd8fdd7a42f7904aeb39136
Tyrah <tyrah96@gmail.com>
2022-07-27 19:54:16 -0700

    re-push

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { Text, StyleSheet, View, Image } from "react-native";
+import { Text, StyleSheet, View, Image, FlatList } from "react-native";
 
 const FeedScreen = ({}) => {
+  const friends = [
+    { name: "Tyrah" },
+    { name: "Lili" },
+    { name: "Emily" },
+    { name: "Ashley" },
+    { name: "Shayla" },
+    { name: "Ivana" },
+  ];
   return (
     <View style={styles.container}>
       <Text style={styles.textStyle}>Friends</Text>
+      <FlatList
+        data={friends}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(friend) => friend.name}
+        renderItem={({ item }) => {
+          return <Text style={styles.bodyText}>{item.name}</Text>;
+        }}
+      />
     </View>
   );
 };
@@ -20,6 +36,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginTop: 40,
     marginLeft: 15,
+  },
+  bodyText: {
+    marginVertical: 50,
   },
   buttons: {
     backgroundColor: "#7F00FF",
