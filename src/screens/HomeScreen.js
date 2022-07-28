4c2056e197b1ac5126049060e534c68e56bc934f
Tyrah <tyrah96@gmail.com>
2022-07-27 18:54:55 -0700

    added yelp api to homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,41 @@
-import React from "react";
-import { Text, StyleSheet, View, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import yelp from "../api/yelp";
 
 const HomeScreen = ({}) => {
+  const [restaurants, setRestaurants] = useState([]);
+
+  //get request from Yelp
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          location: "Seattle, WA",
+        },
+      });
+      setRestaurants(response.data.businesses);
+    } catch (err) {
+      console.log(`The yelp Api was not called:  ${err}`);
+    }
+  };
+
+  //grab search results when first rendered
+  useEffect(() => {
+    searchApi();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}>Home Screen/Feed</Text>
+      <Text style={styles.header}>Home Screen/Feed</Text>
+      <Text>We have found {restaurants.length}</Text>
     </View>
   );
 };
@@ -12,15 +43,17 @@ const HomeScreen = ({}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
     backgroundColor: "#e5e5e5",
   },
-  textStyle: {
-    fontSize: 30,
+  header: {
+    fontSize: 32,
     color: "#141414",
     fontWeight: "bold",
-    fontFamily: "Avenir-Oblique",
+    marginTop: 40,
+    marginLeft: 15,
+  },
+  bodyText: {
+    marginVertical: 50,
   },
   buttons: {
     backgroundColor: "#7F00FF",
