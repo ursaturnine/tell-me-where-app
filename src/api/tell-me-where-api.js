ffe884e532230a505f158792962089999ed8d2c3
Liliana <liliana.m.parra@gmail.com>
2022-08-02 19:58:40 +0200

    fixed base url

--- src/api/tell-me-where-api.js
+++ src/api/tell-me-where-api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://127.0.0.1:5000",
-  baseURL: " https://tell-me-where-backend.herokuapp.com",
+  baseURL: "http://tell-me-where-backend.herokuapp.com",
 });
