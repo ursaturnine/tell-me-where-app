fbf3ba8c754011d04beffa0f3695fd540d56262c
Tyrah <tyrah96@gmail.com>
2022-08-04 00:23:24 -0700

    started code for get friend recs by location

--- src/hooks/useRecs.js
+++ src/hooks/useRecs.js
@@ -1,12 +1,10 @@
 import { useState, useContext } from "react";
 import tellMeWhereApi from "../api/tell-me-where-api";
 import { AuthContext } from "../context/AuthContext";
-import useRecsDisplay from "./useRecsDisplay";
 
 export default () => {
   const [location, setLocation] = useState("");
   const [search, setSearch] = useState("");
-  const [getUserRecs, setRecs, useEffect] = useRecsDisplay();
   const { userID } = useContext(AuthContext);
   const addRecApi = async () => {
     try {
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { Text, StyleSheet, View, Image } from "react-native";
 import InputForm from "../components/InputForm";
+import CustomButton from "../components/CustomButton";
+import tellMeWhereApi from "../api/tell-me-where-api";
+import { AuthContext } from "../context/AuthContext";
+import useRecsDisplay from "../hooks/useRecsDisplay";
 
 const SearchScreen = ({}) => {
+  const [getUserRecs] = useRecsDisplay();
+  const [location, setLocation] = useState("");
+  const { userID } = useContext(AuthContext);
+
+  //get friends ids
+  const getFriendsIds = async () => {
+    const resp = await tellMeWhereApi.get(`users/${userID}`);
+    const friends = resp.data.user.friends.map((friend) => friend.id);
+    getRecsByLocation(friends);
+  };
+
+  // get friends recs with matching locations by ids
+  const getRecsByLocation = (friends) => {
+    const friend_recs = friends.map((friend) => {
+      const resp = tellMeWhereApi.get(`users/${friend}`);
+      console.log(resp);
+      // return resp.data.user.recs;
+    });
+    console.log(friend_recs);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Search Screen</Text>
-      <InputForm placeholder="Search Restaurant..." />
+      <InputForm
+        placeholder="Search Restaurant..."
+        value={location}
+        setValue={setLocation}
+      />
+      <CustomButton
+        onPress={getFriendsIds}
+        text="Tell Me Where"
+        type="PRIMARY"
+      />
     </View>
   );
 };
