08f4b02fefa8e22c9356e1669164468359e5df8f
Tyrah <tyrah96@gmail.com>
2022-08-04 12:56:32 -0700

    completed get friends recs by location functions, have to test after new backend code is deployed

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,18 @@
 import React, { useState, useContext } from "react";
-import { Text, StyleSheet, View, Image } from "react-native";
+import { Text, StyleSheet, View, Image, ScrollView } from "react-native";
 import InputForm from "../components/InputForm";
 import CustomButton from "../components/CustomButton";
 import tellMeWhereApi from "../api/tell-me-where-api";
 import { AuthContext } from "../context/AuthContext";
 import useRecsDisplay from "../hooks/useRecsDisplay";
+import yelpApi from "../api/yelp";
 
 const SearchScreen = ({}) => {
   const [getUserRecs] = useRecsDisplay();
   const [location, setLocation] = useState("");
   const { userID } = useContext(AuthContext);
   const [friendRecs, setFriendRecs] = useState([]);
+  const [noResults, setNoResults] = useState("");
 
   //get friends ids
   const getFriendsIds = async () => {
@@ -19,18 +21,68 @@ const SearchScreen = ({}) => {
     getRecsByLocation(friends);
   };
 
+  //display friend recs
+  const displayFriendRecs = async (id) => {
+    const resp = await tellMeWhereApi.get(`users/${userID}`);
+    console.log(resp.data.user.recs);
+    // setFriendRecs(resp.data.user.recs)
+  };
+
   // get friends recs with matching locations by ids
   const getRecsByLocation = async (friends) => {
-    console.log(friends);
+    // console.log(friends);
+    //for each friend id, get friend.recs
     const friend_recs = Promise.all(
       friends.map(async (friend) => {
-        console.log(friend);
+        // console.log(friend);
         let resp = await tellMeWhereApi.get(`users/${friend}`);
         return resp.data.user.recs;
       })
     );
-    const all_friend_recs = await friend_recs;
-    console.log(all_friend_recs);
+    //if location in friend.recs.location_city or in friend.recs.location_state: display!
+    const all_friends = await friend_recs;
+    console.log(all_friends);
+    const results = Promise.all(
+      all_friends.map((friend) =>
+        friend.map(async (rec) => {
+          if (rec.location_city) {
+            if (
+              location in rec.location_city ||
+              location in rec.location_state
+            ) {
+              displayFriendRecs(rec.id);
+            }
+          } else if (rec.yelp_id) {
+            //if not, grab location for friend id with yelp API
+            console.log(rec.yelp_id);
+            getLocationWithYelp(rec.yelp_id);
+          } else {
+            setNoResults("");
+          }
+        })
+      )
+    );
+    //display no results message if no results found
+    const friend_location_query =
+      (await results) === "undefined" ? false : results;
+    if (friend_location_query === false) {
+      setNoResults("No Matches");
+    } else {
+      setNoResults("");
+    }
+  };
+
+  //get location with yelp id
+  const getLocationWithYelp = async (id) => {
+    const resp = await yelpApi.get(`/search`);
+    if (id in resp.data.id) {
+      if (
+        resp.data.location.city === location ||
+        resp.data.location.state === location
+      ) {
+        displayFriendRecs(id);
+      }
+    }
   };
 
   return (
@@ -46,6 +98,12 @@ const SearchScreen = ({}) => {
         text="Tell Me Where"
         type="PRIMARY"
       />
+      <Text>{noResults ? noResults : ""}</Text>
+      <View>
+        <ScrollView>
+          <View>{friendRecs}</View>
+        </ScrollView>
+      </View>
     </View>
   );
 };
